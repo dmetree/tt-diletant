cf0d5c225f3f4d0a3ff3a7d1424e13002821bdd6
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-04-21 10:58:45 +0200

    Updates

---
+++ precache-manifest.adb72e39cba8ae36bf440f9f2c098a86.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1856b124e05030f3b1954a9e035cd6",
+    "url": "/tt-diletant/index.html"
+  },
+  {
+    "revision": "ca2ec3718b09b68e0185",
+    "url": "/tt-diletant/static/css/main.50c374d3.chunk.css"
+  },
+  {
+    "revision": "88590d309db1de3a048e",
+    "url": "/tt-diletant/static/js/2.f086514e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tt-diletant/static/js/2.f086514e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2ec3718b09b68e0185",
+    "url": "/tt-diletant/static/js/main.812ce49f.chunk.js"
+  },
+  {
+    "revision": "f1d8108f495b3ee77542",
+    "url": "/tt-diletant/static/js/runtime-main.f4966cf9.js"
+  },
+  {
+    "revision": "4515ec173208c6b4c0b35a3dafc8f108",
+    "url": "/tt-diletant/static/media/12+.4515ec17.svg"
+  },
+  {
+    "revision": "a00b825aa26a8a09f93b0820b52caf62",
+    "url": "/tt-diletant/static/media/FB.a00b825a.svg"
+  },
+  {
+    "revision": "ae697b454ebb6a20a5000f98dda316b4",
+    "url": "/tt-diletant/static/media/INST.ae697b45.svg"
+  },
+  {
+    "revision": "8a2a0f5eb1a0a4c5ffaef6feab1ffd43",
+    "url": "/tt-diletant/static/media/OK.8a2a0f5e.svg"
+  },
+  {
+    "revision": "1fc524a22c99e8d63393ecfe238e3d35",
+    "url": "/tt-diletant/static/media/ProximaNova-Regular.1fc524a2.ttf"
+  },
+  {
+    "revision": "a8ea564afc114e1a061cb9ef27a0560b",
+    "url": "/tt-diletant/static/media/ProximaNova-Regular.a8ea564a.woff"
+  },
+  {
+    "revision": "8feb512e78c18175c552af2be6ae2e02",
+    "url": "/tt-diletant/static/media/ProximaNova-Semibold.8feb512e.woff"
+  },
+  {
+    "revision": "df8c626474a73ab7a8b511655597c7c4",
+    "url": "/tt-diletant/static/media/ProximaNova-Semibold.df8c6264.ttf"
+  },
+  {
+    "revision": "14a221c922ff4e6c3177240c9e664236",
+    "url": "/tt-diletant/static/media/Spectral-ExtraBold.14a221c9.woff"
+  },
+  {
+    "revision": "5c6e977e73565bccbd5d669595ae610b",
+    "url": "/tt-diletant/static/media/Spectral-ExtraBold.5c6e977e.ttf"
+  },
+  {
+    "revision": "34235a3610ac34a656414bd21e0e1587",
+    "url": "/tt-diletant/static/media/Spectral-Regular.34235a36.ttf"
+  },
+  {
+    "revision": "6887c18cd9b7011f7cc7856faad8390c",
+    "url": "/tt-diletant/static/media/Spectral-Regular.6887c18c.woff"
+  },
+  {
+    "revision": "5a8480e185af2095245888c52e70c6a2",
+    "url": "/tt-diletant/static/media/Spectral-SemiBoldItalic.5a8480e1.woff"
+  },
+  {
+    "revision": "92141391dd8cf237951712aaf6f4803e",
+    "url": "/tt-diletant/static/media/Spectral-SemiBoldItalic.92141391.ttf"
+  },
+  {
+    "revision": "301fefbd842e9a4f63ca0ec201fcdaa8",
+    "url": "/tt-diletant/static/media/TWI.301fefbd.svg"
+  },
+  {
+    "revision": "8907d35441822e33e8d9013813bf1c17",
+    "url": "/tt-diletant/static/media/VK.8907d354.svg"
+  },
+  {
+    "revision": "b7146a968247961016becb80435bb6b7",
+    "url": "/tt-diletant/static/media/YT.b7146a96.svg"
+  },
+  {
+    "revision": "10a242d1985cd189cf79a178f31ca562",
+    "url": "/tt-diletant/static/media/add_mob.10a242d1.png"
+  },
+  {
+    "revision": "a9465ed968ea0c531b4b16a16cf73601",
+    "url": "/tt-diletant/static/media/add_tablet.a9465ed9.png"
+  },
+  {
+    "revision": "3bec71ae9e4b7a8e8e859ee0c94ead31",
+    "url": "/tt-diletant/static/media/arrow-d.3bec71ae.svg"
+  },
+  {
+    "revision": "8454701747d84fc76eed56707a0a15a9",
+    "url": "/tt-diletant/static/media/close.84547017.svg"
+  },
+  {
+    "revision": "27c6963a4452383e87e4f3df4aaf0548",
+    "url": "/tt-diletant/static/media/filter.27c6963a.svg"
+  },
+  {
+    "revision": "b52c3ab327ab19a6867776da4e9190c6",
+    "url": "/tt-diletant/static/media/img-1.b52c3ab3.png"
+  },
+  {
+    "revision": "6a784263742218773121e40a7f3dbdb2",
+    "url": "/tt-diletant/static/media/img-2.6a784263.png"
+  },
+  {
+    "revision": "d58ed3304d627775959fb13b3a6d7813",
+    "url": "/tt-diletant/static/media/logo.d58ed330.svg"
+  },
+  {
+    "revision": "437a5d0bef0abf856a0990985b1a82d9",
+    "url": "/tt-diletant/static/media/separator.437a5d0b.svg"
+  },
+  {
+    "revision": "c7635ab6b61c4ab2c56534e9aa3b6b4d",
+    "url": "/tt-diletant/static/media/separator_mobile.c7635ab6.svg"
+  }
+]);
