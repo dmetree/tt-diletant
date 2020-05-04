0d5793768fe9f251ab74e79698f32b7e7a4fd479
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-05-04 09:53:45 +0200

    Updates

--- precache-manifest.5ccc9bd3f1ddfddcca870f8b19141cba.js
+++ precache-manifest.5ccc9bd3f1ddfddcca870f8b19141cba.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "391acc618af497b049dbd0746e6625b3",
+    "revision": "2be67d9663d3ae2f2dedf98c7c842ecd",
     "url": "/tt-diletant/index.html"
   },
   {
-    "revision": "2d890e3df3daf4889d64",
+    "revision": "ed21dc98db34539a8a86",
     "url": "/tt-diletant/static/css/main.0a437488.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tt-diletant/static/js/2.939f9422.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d890e3df3daf4889d64",
-    "url": "/tt-diletant/static/js/main.5978aba4.chunk.js"
+    "revision": "ed21dc98db34539a8a86",
+    "url": "/tt-diletant/static/js/main.ccb81fea.chunk.js"
   },
   {
     "revision": "f1d8108f495b3ee77542",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tt-diletant/precache-manifest.9a1575d2d5b9fffa18999d863283cf05.js"
+  "/tt-diletant/precache-manifest.5ccc9bd3f1ddfddcca870f8b19141cba.js"
 );
 
 self.addEventListener('message', (event) => {
